16ac9c7cc1f243018233aa650ca7202683aa4f2e
omarakamal <omaraakamal@gmail.com>
2026-01-07 20:14:52 +0300

    update

--- controllers/book.routes.js
+++ controllers/book.routes.js
@@ -1,4 +1,5 @@
 const Author = require('../models/Author')
+const Book = require('../models/Book')
 
 const router = require('express').Router()
 
@@ -6,6 +7,11 @@ const router = require('express').Router()
 router.get('/create', async(req,res)=>{
     const allAuthors = await Author.find()
     res.render('create-book.ejs',{allAuthors:allAuthors})
+})
+
+router.post('/create', async (req,res)=>{
+    const createdBook = await Book.create(req.body)
+    res.redirect('/books/create')
 })
 
 
