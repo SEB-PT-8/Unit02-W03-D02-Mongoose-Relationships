a2f8247d5285aabb04f475f30882afd2aaa9ab77
omarakamal <omaraakamal@gmail.com>
2026-01-07 20:02:20 +0300

    update

--- controllers/book.routes.js
+++ controllers/book.routes.js
@@ -1,5 +1,12 @@
+const Author = require('../models/Author')
+
 const router = require('express').Router()
 
+
+router.get('/create', async(req,res)=>{
+    const allAuthors = await Author.find()
+    res.render('create-book.ejs',{allAuthors:allAuthors})
+})
 
 
 module.exports = router
